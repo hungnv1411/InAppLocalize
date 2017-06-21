8d932a1c807b44d7de31761bb829075207af44a3
Nguyen Hung <nvhungiph@gmail.com>
2017-06-21 23:11:18 +0700

    init project

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Nguyen Hung on 6/21/17.
+//  Copyright © 2017 Nguyen Hung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Source/InAppLocalize/InAppLocalize.h
@@ -0,0 +1,19 @@
+//
+//  InAppLocalize.h
+//  InAppLocalize
+//
+//  Created by Nguyen Hung on 6/21/17.
+//  Copyright © 2017 Nguyen Hung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for InAppLocalize.
+FOUNDATION_EXPORT double InAppLocalizeVersionNumber;
+
+//! Project version string for InAppLocalize.
+FOUNDATION_EXPORT const unsigned char InAppLocalizeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InAppLocalize/PublicHeader.h>
+
+
